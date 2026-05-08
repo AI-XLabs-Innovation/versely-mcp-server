63714bd48840002eced9f96c9644845d02e4b1e6
Sumit Goyal <goyalsumit651@gmail.com>
2026-05-08 14:41:57 +0530

    tool schema updated

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ export function buildServer(config: Config, client: VerselyClient): Server {
       description: t.description,
       inputSchema: zodToJsonSchema(t.inputSchema, {
         $refStrategy: "none",
-        target: "openApi3",
+        target: "jsonSchema2019-09",
       }) as { type: "object"; [k: string]: unknown },
     })),
   }));
